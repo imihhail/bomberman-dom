7bbf952d1645de06979fc0f652974703e098ce67
The-Estonian <Zaar2213@gmail.com>
2024-07-01 13:51:50 +0300

    added basic tic tac toe game to the project

--- frontend/src/items/Games/Games.jsx
+++ frontend/src/items/Games/Games.jsx
@@ -7,6 +7,7 @@ import tetris from './GamePool/Tetris/tetris.gif';
 import tetris2 from './GamePool/Tetris2/tetris2.gif';
 import bomberman from './GamePool/Bomberman/bomberman.gif';
 import Arkanoid from './GamePool/Arkanoid/arkanoid.gif';
+import TicTacToePic from './GamePool/Tic-Tac-Toe/ticpic.jpg';
 
 const Games = () => {
   const [gameSelected, setGameSelected] = useState(false);
@@ -64,6 +65,13 @@ const Games = () => {
             className={styles.gameLink}
           >
             <img className={styles.gameIcon} src={Arkanoid} />
+          </Link>
+          <Link
+            onClick={handleGameSelect}
+            to={'/games/tictactoe'}
+            className={styles.gameLink}
+          >
+            <img className={styles.gameIcon} src={TicTacToePic} />
           </Link>
         </div>
       )}
